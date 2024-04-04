c406311afac7baf781d0948dd125a9e6480c2274
WhyK <windchimeyk@gmail.com>
2024-04-04 15:10:52 +0900

    feat: complete to chapter 15

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Next.jsの復習を行なうため
 
 ## 参考資料
 - [Learn Next.js | Next.js](https://nextjs.org/learn/dashboard-app)
+- [next-learn/dashboard/final-example at main · vercel/next-learn](https://github.com/vercel/next-learn/tree/main/dashboard/final-example)
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -109,5 +111,24 @@ export const deleteInvoice = async (id: string) => {
     return {
       message: 'Error delete invoice',
     };
+  }
+};
+
+export const authenticate = async (
+  prevState: string | undefined,
+  formData: FormData,
+) => {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 };
